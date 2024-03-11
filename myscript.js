66298384f0abb448db1bab0f83951b319a0d74e9
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2024-03-11 22:54:33 +0330

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,14 +1,6 @@
-let count = 100
-   for (let count = 1;count <= 10;count++){
-      //inside for loop brackets ,count value starts from 1
-      console.log("count value inside loop is ",count);
-   }
-   //outside for loop brackets ,count value is 100
-   console.log("count value after loop is",count);
-
-   if(count == 100){
-      //inside if brackets ,count value is 50
-      let count = 50;
-      console.log("count inside if block",count);
-   }
-   console.log(count);
+let income = 100000
+   const INTEREST_RATE = 0.08
+   income += 50000 // mutable
+   console.log("changed income value is ",income)
+   INTEREST_RATE += 0.01
+   console.log("changed rate is ",INTEREST_RATE) //Error: not mutable
