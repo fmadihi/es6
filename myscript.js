3c337474583335db529df194ea0f8d9311a7c106
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2024-03-11 22:49:19 +0330

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,3 +1,14 @@
-let no = 10; 
-no = 20; 
-console.log(no);
+let count = 100
+   for (let count = 1;count <= 10;count++){
+      //inside for loop brackets ,count value starts from 1
+      console.log("count value inside loop is ",count);
+   }
+   //outside for loop brackets ,count value is 100
+   console.log("count value after loop is",count);
+
+   if(count == 100){
+      //inside if brackets ,count value is 50
+      let count = 50;
+      console.log("count inside if block",count);
+   }
+   console.log(count);
