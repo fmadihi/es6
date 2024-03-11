2c34e341bd8272fc6d7590ffe863c565702228ce
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2024-03-11 23:49:33 +0330

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,10 +1,9 @@
-let income = 100000
-   const INTEREST_RATE = 0.08
-   income += 50000 // mutable
-   console.log("changed income value is ",income)
-   INTEREST_RATE += 0.01
-   console.log("changed rate is ",INTEREST_RATE) //Error: not mutable
+const DEPT_NOS = [10,20,30,50]
+   DEPT_NOS.push(40)
+   console.log('dept numbers is ',DEPT_NOS)
 
-
-
-
+   const EMP_IDS = [1001,1002,1003]
+   console.log('employee ids',EMP_IDS)
+   //re assigning variable employee ids
+   EMP_IDS = [2001,2002,2003]
+   console.log('employee ids after changing',EMP_IDS)
